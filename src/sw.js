9914a517386da25174647ba1dd09be40e090f322
xna <xna00@qq.com>
2022-04-15 13:10:55 +0800

    feat: sw

---
+++ src/sw.js
@@ -0,0 +1,39 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+const PRESHARED_AUTH_HEADER_KEY = "X-Custom-PSK";
+const PRESHARED_AUTH_HEADER_VALUE = "sfiejhr9p8quw";
+
+async function handleRequest(request) {
+  const { url, headers } = request;
+  const psk = headers.get(PRESHARED_AUTH_HEADER_KEY);
+
+  if (psk !== PRESHARED_AUTH_HEADER_VALUE) {
+    return new Response("", {
+      status: 403,
+    });
+  }
+
+  const { pathname } = new URL(url);
+  const raw = pathname.replace(/-/g, "+").replace(/_/g, "/").slice(1);
+
+  try {
+    new URL(atob(raw));
+  } catch (e) {
+    return new Response(e, { status: 400 });
+  }
+
+  const tmp = atob(raw);
+
+  const req = {
+    ...request,
+    url: tmp,
+  };
+
+  return fetch(new Request(tmp, req));
+}
